0fa8b244c0537d540d443ecb8476e018c9911790
Hayden Nier <hayden518@gmail.com>
2024-05-13 14:46:30 -0600

    v2

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,13 +1,16 @@
 
 import {
+  SortingState,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 import { Boulder } from './sendData/boulders'
 import { useSendContext } from './contexts/SendContext'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const columnHelper = createColumnHelper<Boulder>()
 
@@ -15,15 +18,18 @@ const columns = [
   columnHelper.accessor('name', {
     cell: info => info.getValue(),
     footer: info => info.column.id,
+    enableSorting: true,
   }),
   columnHelper.accessor('date', {
     id: 'date',
     cell: info => <i>{info.getValue()}</i>,
     footer: info => info.column.id,
+    enableSorting: true,
   }),
   columnHelper.accessor('grade', {
     cell: info => info.renderValue(),
     footer: info => info.column.id,
+    enableSorting: true,
   })
 ]
 
@@ -37,25 +43,48 @@ const StyledTh = styled.th`
   text-align: start;
   text-transform: capitalize;
 `
-
+const SORT_ICONS: Record<string, JSX.Element> = {
+  asc: <span>&uarr;</span>,
+  desc: <span>&darr;</span>,
+}
 export const Table = () => {
+  const [sorting, setSorting] = useState<SortingState>([]);
   const { boulders } = useSendContext();
   const table = useReactTable({
     data: boulders,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  })
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+
   return (
     <StyledTable>
       <thead>
         {table.getHeaderGroups().map(headerGroup => (
           <tr key={headerGroup.id}>
             {headerGroup.headers.map(header => (
-              <StyledTh key={header.id}>
+              <StyledTh key={header.id}
+                onClick={header.column.getToggleSortingHandler()}
+                style={{
+                  ...(header.column.getCanSort()
+                    ? {
+                      cursor: 'pointer',
+                      userSelect: 'none',
+                    }
+                    : ''),
+                }}
+              >
                 {flexRender(
                   header.column.columnDef.header,
                   header.getContext()
                 )}
+                <span>{SORT_ICONS[header.column.getIsSorted() as string] ?? null}
+                </span>
               </StyledTh>
             ))}
           </tr>
@@ -72,6 +101,6 @@ export const Table = () => {
           </tr>
         ))}
       </tbody>
-    </StyledTable>
+    </StyledTable >
   )
 }
