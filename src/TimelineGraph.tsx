02e6145255791af873d236af5725f9cc5177f9cd
Hayden Nier <hayden518@gmail.com>
2024-05-13 15:02:40 -0600

    data

--- src/TimelineGraph.tsx
+++ src/TimelineGraph.tsx
@@ -11,6 +11,8 @@ export const TimelineGraph = () => {
       <XAxis dataKey="year" />
       <YAxis />
       <Legend />
+      <Bar dataKey="V4" stackId="a" fill="#a77" />
+      <Bar dataKey="V5" stackId="a" fill="#77a" />
       <Bar dataKey="V6" stackId="a" fill="#7aa" />
       <Bar dataKey="V7" stackId="a" fill="#a7a" />
       <Bar dataKey="V8" stackId="a" fill="#7a7" />
