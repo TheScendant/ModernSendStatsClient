16383892619ed88c4c50af5d00fbaa26bd4647d5
Hayden Nier <hayden518@gmail.com>
2024-05-15 16:05:45 -0600

    responsive container (#4)

--- src/PyramidGraph.tsx
+++ src/PyramidGraph.tsx
@@ -1,5 +1,6 @@
 import {
-  BarChart, Bar, XAxis, YAxis, Legend
+  BarChart, Bar, XAxis, YAxis, Legend,
+  ResponsiveContainer,
 } from 'recharts';
 import { useSendContext } from './contexts/SendContext';
 
@@ -9,14 +10,16 @@ export const PyramidGraph = () => {
   return (
     <>
       <h1>Grade Pyramid</h1>
-      <BarChart width={1000} height={500} data={pyramidData} >
-        <XAxis dataKey="grade" />
-        <YAxis />
-        <Legend />
-        <Bar dataKey="2022" stackId="a" fill="rgb(189, 227, 160)" />
-        <Bar dataKey="2023" stackId="a" fill="rgb(147, 233, 190)" />
-        <Bar dataKey="2024" stackId="a" fill="rgb(109, 235, 227)" />
-      </BarChart>
+      <ResponsiveContainer width="80%" height="80%">
+        <BarChart data={pyramidData} >
+          <XAxis dataKey="grade" />
+          <YAxis />
+          <Legend />
+          <Bar dataKey="2022" stackId="a" fill="rgb(189, 227, 160)" />
+          <Bar dataKey="2023" stackId="a" fill="rgb(147, 233, 190)" />
+          <Bar dataKey="2024" stackId="a" fill="rgb(109, 235, 227)" />
+        </BarChart>
+      </ResponsiveContainer>
     </>
   )
 }
